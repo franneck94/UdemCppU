4002cac2dcb5c10dcea5b59437847397571d4b4f
franneck94 <schaffranek.jan@web.de>
2022-09-06 18:57:33 +0200

    up

--- 14_Cpp20Features/Cpp20_Span.cc
+++ 14_Cpp20Features/Cpp20_Span.cc
@@ -1,31 +1,32 @@
+#include <array>
+#include <cstdint>
 #include <iostream>
 #include <span>
 #include <vector>
 
+void f(std::span<std::uint32_t> span)
+{
+    for (const auto v : span)
+        std::cout << v << " ";
+    std::cout << std::endl;
+}
+
 int main()
 {
-    // span with static extent
-    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    std::span<int, 9> span_a{arr};
+    static constexpr std::size_t LEN = 9;
 
-    for (auto v : span_a)
-        std::cout << v << " ";
-    std::cout << std::endl;
+    // span with static extent
+    std::uint32_t arr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    auto arr2 = std::array<std::uint32_t, LEN>{0, 1, 2, 3, 4, 5, 6, 7, 8};
+    auto vec = std::vector<std::uint32_t>{0, 1, 2, 3, 4, 5, 6, 7, 8};
+    f(arr1);
+    f(arr2);
+    f(vec);
 
     // spans with dynamic extent
-    std::vector a{0, 1, 2, 3, 4, 5, 6, 7, 8};
-
-    std::span span_b(a.begin() + 1, a.end() - 1);
-
-    for (auto v : span_b)
-        std::cout << v << " ";
-    std::cout << std::endl;
-
-    std::span span_c = span_b.subspan(1, 5);
-
-    for (auto v : span_c)
-        std::cout << v << " ";
-    std::cout << std::endl;
+    auto span = std::span(vec.begin(), vec.end());
+    const auto span2 = span.subspan(1, 5);
+    f(span2);
 
     return 0;
 }
